49a3c685a22690591cd1b4864deba8cd7fbd1bce
olemartinruud <mr.olemartinruud@yahoo.no>
2017-10-04 14:42:50 +0200

    <strong>DENNE ER RÅ!</strong> Har fått laget et skript som kan ta et bilde og lese det inn. Deretter så deler skriptet bildet opp i 8x8 blokker og vi kan endre på disse blokkene. Det er bare å lage sin egen versjon av skriptet å prøve seg fram! Jeg skal prøve å lage skriptet enklere å bruke.

--- python/bilde_til_maple_piecewise.py
+++ python/bilde_til_maple_piecewise.py
@@ -15,14 +15,49 @@ import math
 
 DATA_DIR = os.path.join(os.getcwd(), '../bilder/fourier_bilder/data/')
 IMAGE_DIR = os.path.join(os.getcwd(), '../bilder/fourier_bilder/')
+GENERATED_DIR = os.path.join(os.getcwd(), '../bilder/fourier_bilder/genererte/')
 
-##########################################################################
-IMAGE_DATA_NAME = "rocket_original.json" #### ENDRE PÅ DENNE FOR Å ENDRE DATA FIL
-##########################################################################
+# Height and width of np_array must be divisible by 8
+def array_into_eight_by_eight(np_array):
+    width = np_array.shape[0]
+    height = np_array.shape[1]
+    if(width % 8 > 0 or height % 8 > 0):
+        return None
+    # Les som en bok
+    array_eight_by_eights = [ [] for i in range(int(height/8)) ]
+    array_eight_by_eight_col_offset = 0
 
-def read_datafile(image_data_name):
+    for y_matrix in range(int(width/8)):
+        n1 = np.arange(8*y_matrix, 8*y_matrix+8)
+        for x_matrix in range(int(height/8)):
+            n2 = np.arange(8*x_matrix, 8*x_matrix+8)
+            array_eight_by_eights[array_eight_by_eight_col_offset].append(np_array[n1[:,None], n2[None,:]])
+        array_eight_by_eight_col_offset += 1
+
+    return array_eight_by_eights
+
+def assembly_array_of_eight_by_eight(array_eight_by_eights):
+    rows_eight_by_eight = []
+    for row_eight_by_eight in array_eight_by_eights:
+        rows_eight_by_eight.append(np.concatenate(row_eight_by_eight, axis=1))
+    return np.concatenate(rows_eight_by_eight, axis=0)
+
+def numpyarray_to_image(np_array, filepath):
+    new_image = Image.fromarray(np_array,'L')
+    with open(filepath, 'wb') as image_file:
+        new_image.save(image_file, 'PNG')
+
+def image_to_numpyarray(loaded_image):
+    converted_image_file = loaded_image.convert('L')
+    return np.reshape(np.array(converted_image_file.getdata(), np.dtype(np.uint8)), (-1, converted_image_file.width))
+
+def write_datafile(filepath, np_array):
+    with open(filepath, "w") as json_file:
+        json.dump(np_array.tolist(), json_file)
+
+def read_datafile(filepath):
     res_json = ""
-    with open(os.path.join(DATA_DIR, image_data_name), 'r') as json_file:
+    with open(filepath, 'r') as json_file:
         res_json = json.load(json_file)
     return res_json
 
@@ -109,19 +144,16 @@ def image_array_to_piecewise_metode3(np_array):
     return res + ")"
 
 
-
-
-def main():
-    image_array = read_datafile(IMAGE_DATA_NAME)
+def test_main():
+    image_data_name = "rocket_original.json"
+    image_array = read_datafile(os.path.join(DATA_DIR, image_data_name))
     image_np_array = np.array(image_array, np.dtype(np.uint8))
-    new_image = Image.fromarray(image_np_array,'L')
+    image_name = image_data_name.split('.')[0] + ".png"
+    filepath = os.path.join(IMAGE_DIR, image_name)
 
-    image_name = os.path.splitext(os.path.basename(IMAGE_DATA_NAME))[0] + ".png"
+    numpyarray_to_image(image_np_array, filepath)
 
-    with open(os.path.join(IMAGE_DIR, image_name), 'wb') as image_file:
-        new_image.save(image_file, 'PNG')
-
-    print("Lagret bilde: " + os.path.join(IMAGE_DIR, image_name))
+    print("Lagret bilde: " + filepath)
     print("Piecewise metode 1:")
     print(image_array_to_piecewise_metode1(image_np_array))
     print("Piecewise metode 2:")
@@ -129,6 +161,32 @@ def main():
     print("Piecewise metode 3:")
     print(image_array_to_piecewise_metode3(image_np_array))
 
+def main_main():
+    image_name_ext = "small_black_white_road.jpg"
+    image_name = image_name_ext.split('.')[0]
+
+    np_array = None
+    with Image.open(os.path.join(IMAGE_DIR, image_name_ext)) as big_image:
+        np_array = image_to_numpyarray(big_image)
+
+    array_eight_by_eights = array_into_eight_by_eight(np_array)
+
+    for row_eight_by_eights in array_eight_by_eights:
+        for eight_by_eight in row_eight_by_eights:
+            ###############################
+            # HER KAN VI LAGE EN FOURIERREKKE AV DATAEN OG GENERERE ET NYTT BILDE SOM BLE LAGET AV FOURIERREKKEN
+            ###############################
+            pass
+
+    new_np_array = assembly_array_of_eight_by_eight(array_eight_by_eights)
+
+    numpyarray_to_image(new_np_array, os.path.join(GENERATED_DIR, image_name + ".png"))
+
+    write_datafile(os.path.join(DATA_DIR, image_name + ".json"), np_array)
+    write_datafile(os.path.join(DATA_DIR, image_name + ".gen.json"), new_np_array)
+
+def main():
+    main_main()
 
 if __name__ == '__main__':
     main()
